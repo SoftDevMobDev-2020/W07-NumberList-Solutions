c888a93a729cd01a4c37630dd29904e157d6fdeb
N. A. Ronald <nronald@swin.edu.au>
2020-09-27 11:22:45 +1000

    To end of video, using second approach for the detail activity.

--- app/src/main/java/au/edu/swin/sdmd/w07_numberlist/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w07_numberlist/MainActivity.kt
@@ -1,7 +1,9 @@
 package au.edu.swin.sdmd.w07_numberlist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,14 @@ class MainActivity : AppCompatActivity() {
         val numberList = findViewById<RecyclerView>(R.id.numberList)
 
         val data = IntArray(100) { it }.filter { it % 3 == 0 || it % 5 == 0}
+
+        numberList.adapter = NumberAdapter(data) { showDetail(it) }
+        numberList.layoutManager = LinearLayoutManager(this)
+    }
+
+    fun showDetail(item: Int) {
+        val i = Intent(this, DetailActivity::class.java)
+        i.putExtra("NUMBER", item)
+        startActivity(i)
     }
 }
